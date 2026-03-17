7ff3528fa64ea81419d130ae4496698326310f05
Ramika-N <ramikanimtharu76@gmail.com>
2026-03-17 13:19:41 +0530

    Initial commit

---
+++ app/src/main/java/com/ramika/fixitdirect/activity/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.ramika.fixitdirect.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ramika.fixitdirect.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
