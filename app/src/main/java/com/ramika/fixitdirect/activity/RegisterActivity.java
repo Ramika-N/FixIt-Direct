4507bc6938b7e517e2925358f6f230ee4f67ef66
Ramika-N <ramikanimtharu76@gmail.com>
2026-03-24 01:06:02 +0530

    Registration back end created

--- app/src/main/java/com/ramika/fixitdirect/activity/RegisterActivity.java
+++ app/src/main/java/com/ramika/fixitdirect/activity/RegisterActivity.java
@@ -2,26 +2,54 @@ package com.ramika.fixitdirect.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.ramika.fixitdirect.R;
+import com.ramika.fixitdirect.model.User;
+
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputEditText etName, etEmail, etPassword;
+    private Button btnRegister;
+    private TextView loginLink;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        TextView loginLink = findViewById(R.id.tvLoginLink);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+        loginLink = findViewById(R.id.tvLoginLink);
+
         loginLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,14 +59,85 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
-        Button registerButton = findViewById(R.id.btnRegister);
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
+                registerUser();
             }
         });
+
+    }
+
+    private void registerUser() {
+
+        String name = etName.getText().toString().trim();
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(name)) {
+            etName.setError("Name is required");
+            etName.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(email)) {
+            etEmail.setError("Email is required");
+            etEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Please provide a valid email");
+            etEmail.requestFocus();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            etPassword.setError("Password is required");
+            etPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            etPassword.setError("Password must be at least 6 characters!");
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    String userId = mAuth.getCurrentUser().getUid();
+
+                    firebaseFirestore = firebaseFirestore.getInstance();
+
+                    User user = User.builder().uid(userId).name(name).email(email).build();
+
+                    firebaseFirestore.collection("users").document(userId).set(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(RegisterActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+
+                                    Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                                    startActivity(i);
+                                    finish();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+                                }
+                            });
+
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+        });
+
     }
 }
